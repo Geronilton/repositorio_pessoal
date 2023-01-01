0a21035159d05dd16c0aad2283004b2a429d5754
geronilton <geronilton001@gmail.com>
2023-01-01 19:48:35 -0300

    fiz o projetofuncionario

---
+++ java/projeto_funcionario/src/main/java/com/mycompany/projeto_funcionario/Projeto_funcionario.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_funcionario;
+
+/**
+ *
+ * @author User
+ */
+public class Projeto_funcionario {
+
+    public static void main(String[] args) {
+        funcionario f1 = new funcionario();
+
+        f1.setNome("hugo");
+        f1.setSalario(100);
+      //  f1.setrecebeaumento(50);
+    
+
+    System.out.println("salario atual:" + f1.getSalario());
+    System.out.println("ganho anual:" + f1.getcalculaganhoanual());
+    
+        funcionario f2 = new funcionario ();
+        
+        f2.setNome("hugo");
+        f2.setSalario(100);
+        
+        if (f1 == f2){
+            System.out.println(" Iguais");
+        }else{
+            System.out.println("Diferentes");
+        }
+    }
+}
---
+++ java/projeto_funcionario/src/main/java/com/mycompany/projeto_funcionario/funcionario.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto_funcionario;
+
+/**
+ *
+ * @author User
+ */
+public class funcionario {
+    private String nome;
+    String departamento;
+    private double salario;
+    String dataEntrada;
+    String rg;
+
+    public void  mostra(){
+        System.out.println("Nome" + this.nome);
+        System.out.println("Salario" + this.salario);
+        System.out.println("Departamento" + this.departamento);
+        System.out.println("Data de Entrada" + this.dataEntrada);
+        System.out.println("RG" + this.rg);
+    }
+      
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getdepartamento() {
+        return nome;
+    }
+
+
+    public void setdepartamento(String nome) {
+        this.nome = nome;
+    }
+
+
+    public double getSalario() {
+        return salario;
+    }
+
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public double getcalculaganhoanual (){
+        return salario*12;
+    }
+    public void setrecebeaumento (double aumento ){
+        this.salario = aumento;
+    }
+    
+    public double getrecebeaumento (){
+        return salario;
+    }
+    
+}
